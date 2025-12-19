283529e42d3fb75c95c1162615d4ee8c65f23aa8
Workaccount6447 <sndj86779@gmail.com>
2025-12-19 21:54:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = os.environ.get("API_ID", "37891385")
 API_HASH = os.environ.get("API_HASH", "2d14a3ad7a0ea5ec9167fb1623f56fa2")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 ADMIN = int(os.environ.get("ADMIN", "8420494874"))
-PICS = (os.environ.get("PICS", "")).split()
+PICS = (os.environ.get("PICS", "https://filehosting.kustbotsweb.workers.dev/f/ca44a44d52b04241ae77632b8ec4e484")).split()
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003386051877"))
 
