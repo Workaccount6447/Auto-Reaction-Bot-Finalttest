d7a1ccccf53db6da2ac0105f1427b4d20aa8ebc5
Workaccount6447 <sndj86779@gmail.com>
2025-12-19 22:42:58 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,5 +1,5 @@
 class text(object):
-  START = """<b>{},
+    START = """<b>{},
 
 ɪ ᴀᴍ sɪᴍᴘʟᴇ ʙᴜᴛ ᴘᴏᴡᴇʀꜰᴜʟʟ ᴀᴜᴛᴏ ʀᴇᴀᴄᴛɪᴏɴ ʙᴏᴛ.
 
@@ -7,8 +7,7 @@ class text(object):
 
 <blockquote><b>‣ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ & ᴍᴀᴅᴇ ʙʏ : <a href='https://telegram.me/RoyalityBots'>ʀᴏʏᴀʟɪᴛʏ ʙᴏᴛꜱ</a></b></blockquote>"""
 
-
-  LOG = """👁️‍🗨️ 𝘜𝘚𝘌𝘙 𝘋𝘌𝘛𝘈𝘐𝘓𝘚
+    LOG = """👁️‍🗨️ 𝘜𝘚𝘌𝘙 𝘋𝘌𝘛𝘈𝘐𝘓𝘚
 
 ○ 𝘐𝘋 : <code>{}</code>
 ○ 𝘋𝘊 : {}
@@ -16,15 +15,15 @@ class text(object):
 ○ 𝘜𝘴𝘦𝘳𝘕𝘢𝘮𝘦 : {}
 
 𝘉𝘺 = @{}"""
-  
-      ABOUT = """<b>‣ ᴍʏ ɴᴀᴍᴇ : <a href='https://youtube.com'>ǫᴜɪᴄᴋ ʀᴇᴀᴄᴛɪᴏɴ ʀᴏʙᴏᴛ</a>
+
+    ABOUT = """<b>‣ ᴍʏ ɴᴀᴍᴇ : <a href='https://youtube.com'>ǫᴜɪᴄᴋ ʀᴇᴀᴄᴛɪᴏɴ ʀᴏʙᴏᴛ</a>
 ‣ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ</a>
 ‣ ᴅᴀᴛᴀʙᴀsᴇ : <a href='https://www.mongodb.com/'>ᴍᴏɴɢᴏᴅʙ</a>
 ‣ ʟᴀɴɢᴜᴀɢᴇ : <a href='https://www.python.org/download/releases/3.0/'>ᴘʏᴛʜᴏɴ 𝟹</a>
 ‣ ʙᴏᴛ sᴇʀᴠᴇʀ : <a href='https://www.koyeb.com/'>ᴋᴏʏᴇʙ</a>
 ‣ ᴄʀᴇᴀᴛᴇᴅ ʙʏ : <a href='http://t.me/Royalitysupportrobot'>ʀᴏʏᴀʟɪᴛʏ ʙᴏᴛꜱ</a></b>"""
 
-  HELP = """<b>{},
+    HELP = """<b>{},
 
 ᴛʜɪꜱ ɪꜱ ʀᴇᴀʟʟʏ sɪᴍᴘʟᴇ 😂
 
